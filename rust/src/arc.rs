e7f209aec6153dab4d95310f97d5ee8f1558bcdc
faultypointer <faultypointer@proton.me>
2026-01-14 19:40:10 +0545

    optimized arc

--- rust/src/arc.rs
+++ rust/src/arc.rs
@@ -1,5 +1,6 @@
 use std::{
     cell::UnsafeCell,
+    mem::ManuallyDrop,
     ops::Deref,
     ptr::NonNull,
     sync::atomic::{AtomicUsize, Ordering, fence},
@@ -10,17 +11,17 @@ struct ArcData<T> {
     data_ref_count: AtomicUsize,
     // no of Arcs + Weaks
     alloc_ref_count: AtomicUsize,
-    data: UnsafeCell<Option<T>>,
+    data: UnsafeCell<ManuallyDrop<T>>,
 }
-
 pub struct Arc<T> {
-    weak: Weak<T>,
+    ptr: NonNull<ArcData<T>>,
 }
+unsafe impl<T: Sync + Send> Send for Arc<T> {}
+unsafe impl<T: Sync + Send> Sync for Arc<T> {}
 
 pub struct Weak<T> {
     ptr: NonNull<ArcData<T>>,
 }
-
 // sending Arc to another thread means, T object is shared so T should be Sync
 // also if that arc is the final clone, meaning when it is dropped
 // T object will also be dropped so T should also be Send
@@ -32,30 +33,58 @@ unsafe impl<T: Send + Sync> Sync for Weak<T> {}
 impl<T> Arc<T> {
     pub fn new(data: T) -> Self {
         Self {
-            weak: Weak {
-                ptr: NonNull::from(Box::leak(Box::new(ArcData {
-                    data_ref_count: AtomicUsize::new(1),
-                    alloc_ref_count: AtomicUsize::new(0),
-                    data: UnsafeCell::new(Some(data)),
-                }))),
-            },
+            ptr: NonNull::from(Box::leak(Box::new(ArcData {
+                data_ref_count: AtomicUsize::new(1),
+                alloc_ref_count: AtomicUsize::new(0),
+                data: UnsafeCell::new(ManuallyDrop::new(data)),
+            }))),
         }
     }
 
     pub fn get_mut(arc: &mut Self) -> Option<&mut T> {
-        if arc.weak.data().alloc_ref_count.load(Ordering::Relaxed) == 1 {
-            fence(Ordering::Acquire);
-            let arcdata = unsafe { arc.weak.ptr.as_mut() };
-            let option = arcdata.data.get_mut();
-            let data = option.as_mut().unwrap();
-            Some(data)
-        } else {
-            None
+        if arc
+            .data()
+            .alloc_ref_count
+            .compare_exchange(1, usize::MAX, Ordering::Acquire, Ordering::Relaxed)
+            .is_err()
+        {
+            return None;
+        }
+
+        let is_unique = arc.data().data_ref_count.load(Ordering::Relaxed) == 1;
+        arc.data().alloc_ref_count.store(1, Ordering::Release);
+        if !is_unique {
+            return None;
+        }
+        fence(Ordering::Acquire);
+        unsafe { Some(&mut *arc.data().data.get()) }
+    }
+
+    pub fn downgrade(arc: &Self) -> Weak<T> {
+        let mut n = arc.data().alloc_ref_count.load(Ordering::Relaxed);
+        loop {
+            if n == usize::MAX {
+                std::hint::spin_loop();
+                n = arc.data().alloc_ref_count.load(Ordering::Relaxed);
+                continue;
+            }
+            assert!(n <= usize::MAX / 2);
+            // Acquire synchronises with get_mut's release-store.
+            if let Err(e) = arc.data().alloc_ref_count.compare_exchange_weak(
+                n,
+                n + 1,
+                Ordering::Acquire,
+                Ordering::Relaxed,
+            ) {
+                n = e;
+                continue;
+            }
+            return Weak { ptr: arc.ptr };
         }
     }
 
-    pub fn downgrade(&self) -> Weak<T> {
-        self.weak.clone()
+    fn data(&self) -> &ArcData<T> {
+        unsafe { self.ptr.as_ref() }
     }
 }
 
@@ -81,7 +110,7 @@ impl<T> Weak<T> {
                 n = e;
                 continue;
             }
-            return Some(Arc { weak: self.clone() });
+            return Some(Arc { ptr: self.ptr });
         }
     }
 }
@@ -89,10 +118,7 @@ impl<T> Weak<T> {
 impl<T> Deref for Arc<T> {
     type Target = T;
     fn deref(&self) -> &T {
-        let ptr = self.weak.data().data.get();
-        // Safety: Since there's an Arc to the data,
-        // the data exists and may be shared.
-        unsafe { (*ptr).as_ref().unwrap() }
+        unsafe { &*self.data().data.get() }
     }
 }
 
@@ -107,11 +133,10 @@ impl<T> Clone for Weak<T> {
 
 impl<T> Clone for Arc<T> {
     fn clone(&self) -> Self {
-        let weak = self.weak.clone();
-        if weak.data().data_ref_count.fetch_add(1, Ordering::Relaxed) > usize::MAX / 2 {
+        if self.data().data_ref_count.fetch_add(1, Ordering::Relaxed) > usize::MAX / 2 {
             std::process::abort();
         }
-        Arc { weak }
+        Arc { ptr: self.ptr }
     }
 }
 
@@ -134,18 +159,12 @@ impl<T> Drop for Arc<T> {
         // So every other drop before the final one release and the final one acquires
         // We can use AcqRel on every single fetch_sub but more efficient is to use
         // fence with Acquire on the final drop and Release on every fetch_sub
-        if self
-            .weak
-            .data()
-            .data_ref_count
-            .fetch_sub(1, Ordering::Release)
-            == 1
-        {
+        if self.data().data_ref_count.fetch_sub(1, Ordering::Release) == 1 {
             fence(Ordering::Acquire);
-            let ptr = self.weak.data().data.get();
             unsafe {
-                (*ptr) = None;
+                ManuallyDrop::drop(&mut *self.data().data.get());
             }
+            drop(Weak { ptr: self.ptr })
         }
     }
 }
